46d99147d761df9a78fc127901905417b18a4504
Chelsey_Cc <91199018+xcheng0810@users.noreply.github.com>
2023-03-08 21:52:00 +0100

    Add files via upload

---
+++ SQL/dump.sqlite.sql
@@ -0,0 +1,30 @@
+-- TABLE
+CREATE TABLE customers  (
+  id INT NOT NULL,
+  first_name STRING,
+  last_name STRING,
+  address STRING,
+  PRIMARY KEY (id) 
+);
+CREATE TABLE orders(
+  id INT NOT NULL,
+  order_number INT,
+  customer_id INT,
+  product_id INT,
+  PRIMARY key (id),
+  foreign key (customer_id) references customers(id),
+  foreign key (product_id) references products(id)
+  );
+CREATE TABLE products (
+  id INT NOT NULL,
+  name STRING,
+  price MONEY, stock,
+  PRIMARY KEY (id)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
