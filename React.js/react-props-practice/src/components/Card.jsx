a2d868110935e07d9d3a7e34f10a8aa53b8d923b
Chelsey_Cc <91199018+xcheng0810@users.noreply.github.com>
2023-03-12 20:08:59 +0100

    Add files via upload

---
+++ React.js/react-props-practice/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <p>{props.id}</p>
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Details detailInfo={props.tel} />
+        <Details detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
